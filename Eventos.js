eaa47267b8434edc91f0454c02c51f8a8918bdba
bijj2 <94939732+bijj2@users.noreply.github.com>
2022-01-30 21:45:00 -0300

    Add files via upload

---
+++ Eventos.js
@@ -0,0 +1,23 @@
+var listaParticipantes = ["Gabriel Silva","Mayara Ellen","Davi Araújo"]
+
+    if (listaParticipantes.length < 100){
+    //É possivel cadastrar
+    console.log("É possível cadastrar mais participantes")
+    } else {
+    console.log("Não tem mais espaço na lista")
+    }
+
+
+let idade = 18;
+    if (idade < 18){
+        console.log("É preciso se maior ou igual á 18 anos")
+    } else {
+        console.log("Idade mínima permitida para cadastro")
+    }
+
+let dataEvento = 30/01/2022;
+    if (dataEvento > 30/01/2022){
+        console.log("Não é possível se cadastrar em um evento retroativo")
+    } else {
+        console.log("Cadastro realizado com sucesso")
+    }
